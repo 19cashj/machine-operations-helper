13dac9f5cc4c71c697f0a56cc65f99a278883c2f
19cashj <67474732+19cashj@users.noreply.github.com>
2021-10-25 12:36:00 -0400

    Home Screen Version 1

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator, DrawerContentScrollView } from '@react-navigation/drawer';
 import { HomeScreen } from './components/HomeScreen'
 import { Context } from './Context'
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   const [test, setTest] = useState('')
   return (
       <Context.Provider value={{test, setTest}}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">        
-            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />      
-          </Stack.Navigator>
+          <Drawer.Navigator initialRouteName="Home">        
+            <Drawer.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+          </Drawer.Navigator>
             <StatusBar style="auto" />    
         </NavigationContainer>
       </Context.Provider>
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button, Image, Text } from "react-native";
 import { Context } from '../Context'
+
+export function HomeScreen({ navigation }) {
+    return (
+      <View style={styles.container}>
+          <Image style={styles.logo} source={require('../assets/icon.png')} />
+          <Text style={styles.title}>Machine Operations Helper</Text>
+          <Text style={styles.text}>Swipe right to open navigation</Text>
+          <Image style={styles.arrow}source={require('../assets/arrow.png')} />
+      </View>
+    )
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#c3c3c3',
         fontSize: 12
+    },
+    logo: {
+        width: 199,
+        height: 199,
+        alignSelf: 'center',
+        marginTop: 84
+    },
+    title: {
+        fontSize: 48,
+        width: 383,
+        alignSelf: 'center',
+        textAlign: 'center',
+        padding: 20
+    },
+    text: {
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    arrow: {
+        alignSelf: 'center',
+        marginTop: 20
     }
 });
-
-export function HomeScreen() {
-    return (
-      <View style={styles.container}>
-      </View>
-    )
-}
