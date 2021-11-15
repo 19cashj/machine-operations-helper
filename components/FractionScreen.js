6cfd0b195eaac485d27b243570f1588580d0aa79
19cashj <67474732+19cashj@users.noreply.github.com>
2021-11-15 14:34:08 -0500

    Operations with whole numbers

--- components/FractionScreen.js
+++ components/FractionScreen.js
@@ -1,8 +1,7 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { StyleSheet, View, Image, Text, TextInput, Pressable } from "react-native";
-import { usePrevious } from '../hooks/usePrevious';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Pressable, ScrollView } from "react-native";
 import { Context } from '../Context'
-import { create, all } from 'mathjs'
+import { create, all, equal } from 'mathjs'
 
 const config = { }
 const math = create(all, config)
@@ -11,7 +10,7 @@ function WholeButton(props) {
     const value = (JSON.stringify(props.label) || 0);
     function pressFunction() {
         props.setInput((prev) => {
-            return {whole: (prev.whole || '') + value, fraction: (prev.fraction || '')};
+            return {whole: (prev.whole || '') + value, numerator: prev.numerator, denominator: prev.denominator, operation: prev.operation};
         });
     }
     return (
@@ -22,33 +21,69 @@ function WholeButton(props) {
 function FractionButton(props) {
     const value = (JSON.stringify(props.label) || 0);
     function pressFunction() {
+        function numerator(prev) {
+            return {whole: prev.whole, numerator: (prev.numerator || '') + value, denominator: prev.denominator, operation: prev.operation};
+        }
+        function denominator(prev) {
+            return {whole: (prev.whole), numerator: (prev.numerator), denominator: (prev.denominator || '') + value, operation: prev.operation};
+        }
         props.setInput((prev) => {
-        return {whole: (prev.whole || undefined), fraction: (prev.fraction || '') + value};
-    })}
+            const fractionFunctions = [numerator(prev), denominator(prev)];
+            return fractionFunctions[props.function]; {/* 0 numerator, 1 denominator*/};
+        })}
     return (
         <SmallButton pressFunction={pressFunction} color={props.color} label={props.label} />
     )
 }
 
 function UtilityButton(props) {
-    const value = (JSON.stringify(props.label) || 0);
     function pressFunction() {
-        function clear() {
-            return {whole: undefined, fraction: undefined};
+        function clear(prev) { {/* Bug with clear, cant input new operations */}
+            switch(props.type) {
+                case 'whole':
+                default:
+                    return {whole: undefined, numerator: prev.numerator, denominator: prev.denominator, operation: prev.operation};
+                case 'numerator':
+                    return {whole: prev.whole, numerator: undefined, denominator: prev.denominator, operation: prev.operation};
+                case 'denominator':
+                    return {whole: prev.whole, numerator: prev.numerator, denominator: undefined, operation: prev.operation};
+            }
         }
         function back(prev) {
-            if (prev.whole === undefined) {
-                return {whole: undefined, fraction: (prev.whole || undefined)};
-            }
-            else {
-                if (prev.whole.slice(prev.whole.length - 1, prev.whole.length) == '.') {
-                }
-                return {whole: (prev.whole.substring(0, prev.whole.length - 1) || undefined), fraction: (prev.fraction || undefined)};
+            switch(props.type) {
+                case 'whole':
+                default:
+                    if (prev.whole === undefined && props.prevInput === undefined) {
+                        return {whole: undefined, numerator: prev.numerator, denominator: prev.denominator, operation: prev.operation};
+                    }
+                    else if (prev.whole === undefined && props.prevInput !== undefined) {
+                        props.setPrevInput(()=>{
+                            return {whole: undefined, numerator: undefined, denominator: undefined, operation: undefined}
+                        })
+                        return {whole: props.prevInput.whole, numerator: props.prevInput.numerator, denominator: props.prevInput.denominator, operation: prev.operation};
+                    }
+                    else {
+                        return {whole: (prev.whole.substring(0, prev.whole.length - 1)), numerator: prev.numerator, denominator: prev.denominator, operation: prev.operation};
+                    }
+                case 'numerator':
+                    if (prev.numerator === undefined) {
+                        return {whole: prev.whole, numerator: undefined, denominator: prev.denominator, operation: prev.operation};
+                    }
+                    else {
+                        return {whole: prev.whole, numerator: prev.numerator.substring(0, prev.numerator.length - 1), denominator: prev.denominator, operation: prev.operation};
+                    }
+                case 'denominator':
+                    if (prev.denominator === undefined) {
+                        return {whole: prev.whole, numerator: prev.numerator, denominator: undefined};
+                    }
+                    else {
+                        return {whole: prev.whole, numerator: prev.numerator, denominator: (prev.denominator.substring(0, prev.denominator.length - 1)), operation: prev.operation};
+                    }
             }
         }
         props.setInput((prev) => {
-            const utilityFunctions = [clear(), back(prev)];
-            return utilityFunctions[props.type]; {/* 0 = clear, 1 = back (Used this system so more utilities could be added easily)*/}
+            const utilityFunctions = [clear(prev), back(prev)];
+            return utilityFunctions[props.function]; {/* 0 = clear, 1 = back (Used this system so more utilities could be added easily)*/}
         });
     }
     return (
@@ -57,12 +92,44 @@ function UtilityButton(props) {
 }
 
 function OperationButton(props) {
-    const value = (JSON.stringify(props.label) || 0);
-    function pressFunction() {
-        props.setInput((prev) => {
-            const utilityFunctions = [];
-            return utilityFunctions[props.type];
-        });
+    function pressFunction() { {/* perform an equals function if an operation had already been inputted, put the result of the previous operation into previous input along with the new operation */}
+        function evaluate() {
+            const resultWhole = eval(`${props.prevInput.whole || 0} ${props.prevInput.operation || ''} ${props.input.whole}`);
+            if(props.label == '=') {
+                props.setInput(() => {
+                    return {whole: JSON.stringify(resultWhole), numerator: undefined, denominator: undefined, operation: undefined};
+                });
+                props.setPrevInput(() => {
+                    return {whole: undefined, numerator: undefined, denominator: undefined, operation: undefined};
+                });
+            }
+            else {
+                props.setInput(() => {
+                    return {whole: undefined, numerator: undefined, denominator: undefined, operation: props.label};
+                });
+                props.setPrevInput(() => {
+                    return {whole: JSON.stringify(resultWhole), numerator: undefined, denominator: undefined, operation: props.label};
+                });
+            }
+        }
+        if (props.input.operation || props.label == '=') {
+            if (props.prevInput.whole || props.prevInput.numerator || props.prevInput.denominator) {
+                evaluate()
+            }
+            else {
+                props.setPrevInput(() => {
+                    return {whole: undefined, numerator: undefined, denominator: undefined, operation: undefined};
+                });
+            }
+        }
+        else {
+            props.setPrevInput(() => {
+                return {whole: props.input.whole, numerator: props.input.numerator, denominator: props.input.denominator, operation: props.label};
+            });
+            props.setInput(() => {
+                return {whole: undefined, numerator: undefined, denominator: undefined, operation: props.label};
+            });
+        }
     }
     return (
         <SmallButton pressFunction={pressFunction} color={props.color} label={props.label} />
@@ -89,40 +156,46 @@ function SmallButton(props) {
 }
 
 export function FractionScreen() {
-    const [input, setInput] = useState({whole: undefined, fraction: undefined})
+    const [input, setInput] = useState({whole: undefined, numerator: undefined, denominator: undefined, operation: undefined})
+    const [prevInput, setPrevInput] = useState({whole: undefined, numerator: undefined, denominator: undefined, operation: undefined})
     const [result, setResult] = useState(0);
-    const zeroThruNine = [0,1,2,3,4,5,6,7,8,9]
+    const numPad = [0,7,8,9,4,5,6,1,2,3]
     return (
         <View style={styles.container}>
             <View style={styles.calculator}>
                 <View style={styles.resultField}>
-                    <Text ellipsizeMode='clip' numberOfLines={1} style={styles.resultText}>{(input.whole || 0)}</Text>
+                    <ScrollView horizontal={true}>
+                        <Text ellipsizeMode='clip' numberOfLines={1} style={styles.resultText}>
+                            {prevInput.whole} {prevInput.numerator}{(prevInput.numerator || prevInput.denominator)?'/':undefined}{prevInput.denominator} {prevInput.operation} {(input.whole || 0)} {(input.numerator || '')}{(input.numerator || input.denominator)?'/':''}{input.denominator || ''}
+                        </Text>  
+                    </ScrollView>
                 </View>
                 <View style={styles.top}>
                     <View style={styles.wholeNumbers}>
-                        <UtilityButton label={'C'} color={'red'} setInput={setInput} type={0} />
-                        <UtilityButton label={'<'} color={'blue'} setInput={setInput} type={1} />
-                        {zeroThruNine.map((e) => {
+                        <UtilityButton label={'C'} color={'red'} setInput={setInput} prevInput={prevInput} setPrevInput={setPrevInput} function={0} type={'whole'}/>
+                        <UtilityButton label={'<'} color={'blue'} setInput={setInput} prevInput={prevInput} setPrevInput={setPrevInput} function={1} type={'whole'}/>
+                        {numPad.map((e) => {
                             return <WholeButton label={e} setInput={setInput} key={e} />
                         })}
                         <View style={styles.operations}>
-                            <OperationButton label={'+'} color={'green'} setInput={setInput} type={0} />
-                            <OperationButton label={'-'} color={'darkred'} setInput={setInput} type={0} />
-                            <OperationButton label={'X'} color={'darkblue'} setInput={setInput} type={0} />
-                            <OperationButton label={'/'} color={'orange'} setInput={setInput} type={0} />
+                            <OperationButton label={'+'} color={'green'} prevInput={prevInput} input={input} result={result} setResult={setResult} setInput={setInput} setPrevInput={setPrevInput} />
+                            <OperationButton label={'-'} color={'darkred'} prevInput={prevInput} input={input} result={result} setResult={setResult} setInput={setInput} setPrevInput={setPrevInput} />
+                            <OperationButton label={'*'} color={'darkblue'} prevInput={prevInput} input={input} result={result} setResult={setResult} setInput={setInput} setPrevInput={setPrevInput} />
+                            <OperationButton label={'/'} color={'orange'} prevInput={prevInput} input={input} result={result} setResult={setResult} setInput={setInput} setPrevInput={setPrevInput} />
+                            <OperationButton label={'='} color={'gray'} prevInput={prevInput} input={input} result={result} setResult={setResult} setInput={setInput} setPrevInput={setPrevInput} />
                         </View>
                     </View>
                     <View style={styles.fractionNumbers}>
-                        <UtilityButton label={'C'} color={'red'} setInput={setInput} type={0} />
-                        <UtilityButton label={'<'} color={'blue'} setInput={setInput} type={1} />
-                        {zeroThruNine.map((e) => {
-                            return <FractionButton label={e} setInput={setInput} key={e} />
+                        <UtilityButton label={'C'} color={'red'} prevInput={prevInput} setPrevInput={setPrevInput} setInput={setInput} function={0} type={'numerator'}/>
+                        <UtilityButton label={'<'} color={'blue'} prevInput={prevInput} setPrevInput={setPrevInput} setInput={setInput} function={1} type={'numerator'}/>
+                        {numPad.map((e) => {
+                            return <FractionButton label={e} setInput={setInput} key={e} function={0} />
                         })}
                         <View style={styles.fractionLine} />
-                        <UtilityButton label={'C'} color={'red'} setInput={setInput} type={0} />
-                        <UtilityButton label={'<'} color={'blue'} setInput={setInput} type={1} />
-                        {zeroThruNine.map((e) => {
-                            return <FractionButton label={e} setInput={setInput} key={e} />
+                        <UtilityButton label={'C'} color={'red'} prevInput={prevInput} setPrevInput={setPrevInput} setInput={setInput} function={0} type={'denominator'}/>
+                        <UtilityButton label={'<'} color={'blue'} prevInput={prevInput} setPrevInput={setPrevInput} setInput={setInput} function={1} type={'denominator'}/>
+                        {numPad.map((e) => {
+                            return <FractionButton label={e} setInput={setInput} key={e} function={1} />
                         })}
                     </View>
                 </View>
@@ -143,6 +216,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%',
         alignItems: 'center',
+        justifyContent: 'center',
         padding: 15
     },
     resultField: {
@@ -159,8 +233,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         letterSpacing: 0.25,
         height: 50,
-        width: 300,
         margin: 25,
+        minWidth: 300,
         textAlign: 'right'
     },
     top: {
@@ -174,13 +248,15 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         justifyContent: 'center',
         width: '40%',
-        paddingTop: 68
+        paddingTop: '10%'
     },
     operations: {
-        padding: 35,
+        padding: '15%',
+        paddingTop: '5%',
         flex: 1,
         flexDirection: 'row',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        justifyContent: 'center'
     },
     fractionNumbers: {
         flex: 1,
@@ -191,7 +267,7 @@ const styles = StyleSheet.create({
         paddingTop: 10
     },
     fractionLine : {
-        margin: 20,
+        margin: '1%',
         width: '75%',
         height: 5,
         backgroundColor: 'black'
